9e3718d1a335e94786e8ac37a04ae5d503c3adf0
Sellieee <sellieee@hotmail.com>
2019-07-29 13:27:22 +1000

    added firebase configuration

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
 
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBFNQUsOpMK3CSNrfbBqyh28qfMG27ULBw",
+        authDomain: "project-2-618f5.firebaseapp.com",
+        databaseURL: "https://project-2-618f5.firebaseio.com",
+        projectId: "project-2-618f5",
+        storageBucket: "",
+        messagingSenderId: "212863931586",
+        appId: "1:212863931586:web:0d52e932ab280d42"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
+
+    $("#form-submit").on("click", function (event) {
+        event.preventDefault();
+
+        var trainName = $("#trainName").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrain = $("#trainTime").val().trim();
+        var frequency = $("#frequency").val().trim();
+
+        database.ref().push({
+            name: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+        });
+    });
+
 })
