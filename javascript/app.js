5c79e7ebbd4b69b9ed431499a2f89cd497a61700
Sellieee <sellieee@hotmail.com>
2019-07-29 13:43:38 +1000

    checked and confirm that user input is taken and logged in the console

--- javascript/app.js
+++ javascript/app.js
@@ -13,11 +13,12 @@ $(document).ready(function () {
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    var database = firebase.database();
 
+    var database = firebase.database();
     $("#form-submit").on("click", function (event) {
         event.preventDefault();
 
+        // Grabbing the user input from the form and removing the spaces before and after
         var trainName = $("#trainName").val().trim();
         var destination = $("#destination").val().trim();
         var firstTrain = $("#trainTime").val().trim();
@@ -30,5 +31,15 @@ $(document).ready(function () {
             frequency: frequency,
         });
     });
+
+    database.ref().on("child_added", function (snapshot) {
+        // Test to see if the input values from the form are displaying correctly in console.log
+        console.log(snapshot.val());
+        console.log(snapshot.val().name);
+        console.log(snapshot.val().destination);
+        console.log(snapshot.val().firstTrain);
+        console.log(snapshot.val().frequency);
+    })
+
 
 })
